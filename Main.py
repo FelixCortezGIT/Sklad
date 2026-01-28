869e2233f8f32237b3b89d6e1233647660aaca58
TomasMacula <tomasmacula@live.com>
2026-01-28 08:24:03 +0100

    tvorba menu

--- Main.py
+++ Main.py
@@ -23,12 +23,30 @@ class Queue:
     def show(self):
         print("obsah fronty: ", self.data)
 
-q = Queue(3)
-print(q.is_empty())
-q.enqueue(10)
-q.enqueue(20)
-q.enqueue(30)
-print(q.is_full())
-q.dequeue()
-q.dequeue()
-q.show()
+
+# q = Queue(3)
+# print(q.is_empty())
+# q.enqueue(10)
+# q.enqueue(20)
+# q.enqueue(30)
+# print(q.is_full())
+# q.dequeue()
+# q.dequeue()
+# q.show()
+
+max_queue=int(input("zadaj maximalnu velkost fronty: "))
+q= Queue(max_queue)
+print("\n1) Is Empty - je prazdna")
+print("2) Is Full - je plna")
+print("3) Enqueue - pridat znak")
+print("4) Dequeue - odstranit znak")
+print("5) SHow - zobrazit frontu")
+print("0) koniec")
+
+choice = input("zadaj volbu: ")
+if choice == "1":
+    print("ano, je prazdna" if q.is_empty() else "nie, nie je pazdna")
+elif choice == "2":
+    print("ano, je plna" if q.is_full() else "nie, nie je plna")
+
+
