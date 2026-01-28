8395d09bf06bb0391d3248d869bed33dcd2b1996
TomasMacula <tomasmacula@live.com>
2026-01-28 06:57:08 +0100

    pridane funkcie is_empty a is_full, pridana skuska

--- Main.py
+++ Main.py
@@ -2,5 +2,9 @@ class Queue:
     def __init__(self, max):
         self.max = max
     def is_empty(self):
+        return len(self.data) == 0
+    def is_full(self):
+        return len(self.data) >= max()
 
-    def is_full(selfs):
+print(Queue(50).is_empty)
+print(Queue.is_full)
