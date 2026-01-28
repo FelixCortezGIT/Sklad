55c8b238ce7e7219996f80b92fdc9822f021f948
TomasMacula <tomasmacula@live.com>
2026-01-28 07:21:24 +0100

    pridana funkcia enqueue, osetrena o to aby nepridalo ak je uz fronta plna a opravena aby pridala cely element

--- Main.py
+++ Main.py
@@ -6,7 +6,18 @@ class Queue:
         return len(self.data) == 0
     def is_full(self):
         return len(self.data) >= self.max
+    def enqueue(self, element):
+        if self.is_full():
+            print("fronta je plna, neda sa pridat prvok")
+            return False
+        self.data.append(element)
+        print("pridane: ", element)
+        return True
+
 
 q = Queue(30)
+add = Queue(1)
+print(add.enqueue(20))
+print(add.enqueue(50))
 print(q.is_empty())
 print(q.is_full())
